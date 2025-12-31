64e426f1fa26153480e41b73b1ce9001659456be
khiem <khiemnguyen.hye@gmail.com>
2025-12-31 13:39:56 +0700

    Add cta

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -7,6 +7,7 @@ import { BentoCard, BentoGrid, CARDS } from "@/components/ui/bento-grid";
 import { Button } from "@/components/ui/button";
 import MagicBadge from "@/components/ui/magic-badge";
 import GridBackground from "@/components/GridBackground";
+import { LampContainer } from "@/components/ui/lamb";
 
 
 const LandingPage = () => {
@@ -128,6 +129,27 @@ const LandingPage = () => {
           </BentoGrid>
         </AnimationContainer>
       </MaxWidthWrapper>
+
+      <MaxWidthWrapper className="mt-20 max-w-[100vw] overflow-x-hidden scrollbar-hide">
+                <AnimationContainer delay={0.1}>
+                    <LampContainer>
+                        <div className="flex flex-col items-center justify-center relative w-full text-center">
+                            <h2 className="bg-linear-to-b from-neutral-200 to-neutral-400 py-4 bg-clip-text text-center text-4xl md:text-7xl leading-[1.15]! font-medium font-heading tracking-tight text-transparent mt-8">
+                                Step into the future of link management
+                            </h2>
+                            <p className="text-muted-foreground mt-6 max-w-md mx-auto">
+                                Experience the cutting-edge solution that transforms how you handle your links. Elevate your online presence with our next-gen platform.
+                            </p>
+                            <div className="mt-6">
+                                <Button>
+                                    Get started for free
+                                    <ArrowRightIcon className="w-4 h-4 ml-2" />
+                                </Button>
+                            </div>
+                        </div>
+                    </LampContainer>
+                </AnimationContainer>
+            </MaxWidthWrapper>
     </>
   );
 };
